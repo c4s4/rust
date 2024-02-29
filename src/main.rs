62a73b55875218e0de8e77a04bc4fe0298be138b
Michel Casabianca <mcasabianca@sfbx.io>
2024-02-29 14:47:35 +0100

    Added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+/// Describe project here
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    /// Describe argument here
+    arg: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse command line arguments
+    let args = Cli::parse();
+    // print argument
+    run(args.arg);
+}
+
+/// Describe function here
+fn run(arg: String) {
+    println!("Argument: {}", arg);
 }
